323fa829501a6ae0b57f6ae76034b85f6fe9cf4e
Estefania Agudelo <61963876+eagudelo56@users.noreply.github.com>
2021-04-14 19:08:55 +0200

    Estefania

--- app/src/main/java/com/example/appmaskcycle/MainActivity.kt
+++ app/src/main/java/com/example/appmaskcycle/MainActivity.kt
@@ -3,13 +3,50 @@ package com.example.appmaskcycle
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.example.appmaskcycle.api.Conexion
+import com.example.appmaskcycle.api.DataUsuarios
+import org.jetbrains.anko.doAsync
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+
         Toast.makeText(this,"eeee",Toast.LENGTH_LONG).show()
+
+    }
+
+    private  fun pruebaNom() {
+        val cont =this
+
+        doAsync {
+            val objConexion = Conexion()
+            val sql = "select * from usuarios"
+            val llamada =
+            objConexion.executeQueryUsuario(sql)
+
+            llamada.enqueue(
+                object : Callback<List<DataUsuarios>>{
+                    override fun onFailure(call: Call<List<DataUsuarios>>, t: Throwable) {
+                        Toast.makeText(cont," no funciona",Toast.LENGTH_LONG).show()
+                    }
+
+                    override fun onResponse(
+                        call: Call<List<DataUsuarios>>,
+                        response: Response<List<DataUsuarios>>
+                    ) {
+                        val rel = response.body() as List<DataUsuarios>
+
+                        Toast.makeText(cont,rel[0].nombre,Toast.LENGTH_LONG).show()
+                    }
+                }
+            )
+        }
 
     }
 }
--- app/src/main/java/com/example/appmaskcycle/api/Conexion.kt
+++ app/src/main/java/com/example/appmaskcycle/api/Conexion.kt
@@ -1,11 +1,36 @@
 package com.example.appmaskcycle.api
 
+import okhttp3.OkHttpClient
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
 class Conexion {
 
     private val ip:String = "http://redcapcrd.es/appmaskcycle/"
     private val apiKey:String = "appmaskcycleProyecto2021"
 
+    private val clienteHttp = OkHttpClient().newBuilder()
+         .connectTimeout(15, TimeUnit.SECONDS)
+         .readTimeout(15, TimeUnit.SECONDS)
+         .writeTimeout(15, TimeUnit.SECONDS)
+         .build()
 
+    private fun getRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(ip)
+            .client(clienteHttp)
+            .addConverterFactory(GsonConverterFactory.create())/*todas las librerias json para parsear el json*/
+            .build()/*crear el objecto*/
+    }
+
+    fun executeQueryUsuario(sql:String) : Call<List<DataUsuarios>> {
+        val aux = getRetrofit().create(APIService::class.java).executeQueryApiUsuarios(sql,apiKey)
+        /* llamar al metodo interface le pasamos el sql y api */
+        return aux
+
+    }
 
 
 }
