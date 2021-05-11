46b56d91b9485ce38f562f7ea5c131018790c885
volivic <76037967+volivic@users.noreply.github.com>
2021-05-11 18:09:01 +0200

    creacion de activities necesarios: info, ajustes, manual y registro de usuario

---
+++ app/src/main/java/com/example/appmaskcycle/ManualActivity.kt
@@ -0,0 +1,11 @@
+package com.example.appmaskcycle
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ManualActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_manual)
+    }
+}
