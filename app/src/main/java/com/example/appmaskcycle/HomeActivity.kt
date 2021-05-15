293032b9331e4a4b68c6b035d668451a8307350d
alexgohan8 <alex.moreno.pilar@gmail.com>
2021-05-15 10:29:34 +0200

    añadir clase ConvertirDb.kt

--- app/src/main/java/com/example/appmaskcycle/HomeActivity.kt
+++ app/src/main/java/com/example/appmaskcycle/HomeActivity.kt
@@ -1,6 +1,5 @@
 package com.example.appmaskcycle
 
-import android.app.Activity
 import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
---
+++ app/src/main/java/com/example/appmaskcycle/util/ConvertirDb.kt
@@ -0,0 +1,54 @@
+package com.example.appmaskcycle.util
+
+import java.text.SimpleDateFormat
+import java.util.*
+
+class ConvertirDb {
+
+    companion object {
+        private val sdf = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.ENGLISH)
+
+        fun getCalendarFromString(str:String) : Calendar {
+            val cal = Calendar.getInstance()
+            val aux = sdf.parse(str)
+            if(aux!=null){
+                cal.time = aux
+            }
+            return cal
+        }
+
+        fun getStringFromCalendar(cal:Calendar):String{
+            return sdf.format(cal)
+        }
+
+        /*
+         1 = true
+
+         0 = false
+        * */
+        fun getBooleanFromString(str:String):Boolean{
+            var bool = false
+            if(str == "1"){
+                bool = true
+            }
+            if(str == "0"){
+                bool = false
+            }
+            return bool
+        }
+
+        fun getStringFromBoolean(bool:Boolean) : String{
+            var str = "0"
+            if(bool){
+                str = "1"
+            }
+            if(!bool){
+                str = "0"
+            }
+            return str
+        }
+
+    }
+
+
+}
