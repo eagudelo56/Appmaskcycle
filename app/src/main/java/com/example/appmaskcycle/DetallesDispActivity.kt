a206a96ed8a7fb9d0faa3ded327acd2395e1a62d
volivic <76037967+volivic@users.noreply.github.com>
2021-05-18 18:17:04 +0200

    Botón eliminar mascarillas en uso y disponibles en detalles

--- app/src/main/java/com/example/appmaskcycle/DetallesDispActivity.kt
+++ app/src/main/java/com/example/appmaskcycle/DetallesDispActivity.kt
@@ -1,8 +1,17 @@
 package com.example.appmaskcycle
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.appmaskcycle.api.DataCodigoError
 import com.example.appmaskcycle.clases.DispMasc
+import kotlinx.android.synthetic.main.activity_detalles_disp.*
+import kotlinx.android.synthetic.main.activity_detalles_uso.*
+import org.jetbrains.anko.doAsync
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class DetallesDispActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,6 +30,48 @@ class DetallesDispActivity : AppCompatActivity() {
 
         val mascarilla = DispMasc(id,nombre!!,tipo!!,tInfo!!,lavados,duracion,stock,comentario!!)
 
+        tvDetalleDispNombre.text = mascarilla.nombre
+        tvDetalleDispTipo.text = mascarilla.tipo
+        tvDetalleDispDuracion.text = mascarilla.duracion.toString()
+        tvDetalleDispStock.text = mascarilla.stock.toString()
+        tvDetalleDispLavados.text = mascarilla.lavados.toString()
+        tvDetalleDispComentario.text = mascarilla.comentario
 
+
+        btnDetalleDispEliminar.setOnClickListener{
+            elimDisp(mascarilla)
+        }
+
+
+    }
+
+    private fun elimDisp(mascarilla:DispMasc){
+        val con:Context = this
+        doAsync {
+            val llamada = mascarilla.deleteDispMasc(mascarilla.id)
+            llamada.enqueue(object:Callback<DataCodigoError>{
+                override fun onFailure(call: Call<DataCodigoError>, t: Throwable) {
+                    Toast.makeText(con, "Error al conectar con el servidor", Toast.LENGTH_SHORT).show()
+                }
+                override fun onResponse(
+                    call: Call<DataCodigoError>,
+                    response: Response<DataCodigoError>
+                ) {
+                    val respuesta = response.body()
+                    if(respuesta!=null){
+                        val codigo = respuesta.codigoError
+                        if(codigo==1){
+                            finish()
+                            Toast.makeText(con, "Mascarilla eliminada correctamente", Toast.LENGTH_SHORT).show()
+                        }
+                        else{
+                            Toast.makeText(con, "Error al eliminar mascarilla", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
+
+            })
+
+        }
     }
 }
--- app/src/main/java/com/example/appmaskcycle/DetallesUsoActivity.kt
+++ app/src/main/java/com/example/appmaskcycle/DetallesUsoActivity.kt
@@ -1,10 +1,18 @@
 package com.example.appmaskcycle
 
+import android.content.Context
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.appmaskcycle.api.DataCodigoError
+import com.example.appmaskcycle.clases.DispMasc
 import com.example.appmaskcycle.clases.UsoMasc
 import com.example.appmaskcycle.util.ConvertirDb
 import kotlinx.android.synthetic.main.activity_detalles_uso.*
+import org.jetbrains.anko.doAsync
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class DetallesUsoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,12 +37,44 @@ class DetallesUsoActivity : AppCompatActivity() {
         val mascarilla = UsoMasc(id,nombre!!,tipo!!,tInfo!!,
             inicioCal,activa,horasVidaCal,finalCal,lavados)
 
-
         tvDetalleUsoNombre.text = mascarilla.nombre
         tvDetalleUsoTipo.text = mascarilla.tipo
         tvDetalleUsoTinfo.text = mascarilla.tInfo
         tvDetalleUsoHorasVida.text = mascarilla.getHoraVformato()
         tvDetalleUsoLavados.text = mascarilla.lavados.toString()
 
+        btnDetalleEliminar.setOnClickListener{
+            elimUso(mascarilla)
+        }
+    }
+
+    private fun elimUso(mascarilla: UsoMasc){
+        val con: Context = this
+        doAsync {
+            val llamada = mascarilla.deleteUsoMasc(mascarilla.id)
+            llamada.enqueue(object: Callback<DataCodigoError> {
+                override fun onFailure(call: Call<DataCodigoError>, t: Throwable) {
+                    Toast.makeText(con, "Error al conectar con el servidor", Toast.LENGTH_SHORT).show()
+                }
+                override fun onResponse(
+                    call: Call<DataCodigoError>,
+                    response: Response<DataCodigoError>
+                ) {
+                    val respuesta = response.body()
+                    if(respuesta!=null){
+                        val codigo = respuesta.codigoError
+                        if(codigo==1){
+                            finish()
+                            Toast.makeText(con, "Mascarilla eliminada correctamente", Toast.LENGTH_SHORT).show()
+                        }
+                        else{
+                            Toast.makeText(con, "Error al eliminar mascarilla", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
+
+            })
+
+        }
     }
 }
